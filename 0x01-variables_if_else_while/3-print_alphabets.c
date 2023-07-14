2fa5e2abd7236ea6f2a37041320c50f6ea7763c6
Minabello <minabello1994@gmail.com>
2023-07-14 11:47:16 -0700

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase ,and uppercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}

7f7d472407816cd930aaef7c4cf178dfb593b9cf
Minabello <minabello1994@gmail.com>
2023-07-14 15:55:48 -0700

    from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}

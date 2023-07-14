4c00014632da6fe4a69b68b462e9070cc32fe275
Minabello <minabello1994@gmail.com>
2023-07-14 09:52:14 -0700

    positive negative zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Determine if a random is positive , negative or zero.
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int n;

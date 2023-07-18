ffa52e812a83b17c8cbd05390a9f8a863b4c7875
Minabello <minabello1994@gmail.com>
2023-07-18 10:22:49 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar .
  *
@@ -8,6 +8,6 @@ int main(void)
 {
 	char x[] = "_putchar";
 
-	printf("%s \n", x);
+	printf("%s\n", x);
 	return (0);
 }

d332d10ff37036f66ae133d02dd435184c2de1ad
Minabello <minabello1994@gmail.com>
2023-07-20 16:19:35 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,17 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else if ((a % 5) == 0 && (a % 3) != 0)
+		else if ((a % 5) == 0 && (a % 3) != 0 && a != 100)
 		{
 			printf("Buzz ");
 		}
 		else if ((a % 3) == 0 && (a % 5) == 0)
 		{
 			printf("FizzBuzz ");
+		}
+		else if (a == 100)
+		{
+			printf("Buzz");
 		}
 		else
 		{

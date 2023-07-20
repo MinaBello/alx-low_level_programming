5c71e118f92083fe0e8a9fa696da42204ecebd4c
Minabello <minabello1994@gmail.com>
2023-07-20 15:40:32 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print square of the number by #.
+ * @size: integar input.
+ * Return: 0 on success
+ */
+void print_square(int size)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0 ; a < size ; a++)
+	{
+		while (b < size)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		b = 0;
+	}
+}
+

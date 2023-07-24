2542a9e1e17fc83c364ba7ef0d2df874af85c1ff
Minabello <minabello1994@gmail.com>
2023-07-24 16:52:41 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - print every other character.
+ * @str: input pointer type char
+ * Return: 0 on succcess
+ */
+void puts_half(char *str)
+{
+	int x = 0;
+	int y;
+
+	while ((*(str + x)) != '\0')
+	{
+		x++;
+	}
+	if (x % 2 == 0)
+	{
+		for (y = x / 2; y <= (x - 1); y++)
+		{
+			_putchar(*(str + y));
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = (x - 1) / 2; y <= x - 1; y++)
+		{
+			_putchar(*(str + y));
+		}
+		_putchar('\n');
+	}
+}

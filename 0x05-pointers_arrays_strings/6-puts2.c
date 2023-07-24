bb4110abe095c11358bf2d4124f0d330084fae11
Minabello <minabello1994@gmail.com>
2023-07-24 16:33:07 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - print every other character.
+ * @str: input pointer type char
+ * Return: 0 on succcess
+ */
+void puts2(char *str)
+{
+	int x = 0;
+	int y;
+
+	while ((*(str + x)) != '\0')
+	{
+		x++;
+	}
+	for (y = 0; y < x - 1 ; y += 2)
+	{
+		_putchar(*(str + y));
+	}
+	_putchar('\n');
+}

3303df13ca5d93a70223406b17dbc2eb14930bff
MinaBello <minabello1994@gmail.com>
2024-03-22 21:48:18 +0200

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: pointer to array of cahrs
+ * Return: print string to output
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}

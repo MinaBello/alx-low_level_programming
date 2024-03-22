3746dfb99ba098f036f0f9cd1e65ac26df0a597a
MinaBello <minabello1994@gmail.com>
2024-03-22 23:19:32 +0200

    Power Number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion: function the make number power another number
+ * @x: input integar as Base number
+ * @y: input integar as power number
+ * Return: power of this numbers
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (x == 0 || y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}

010080c82be013652dd87ce635e73c029f808377
MinaBello <minabello1994@gmail.com>
2024-04-05 19:03:46 +0200

    Square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int natural_square(int x, int y);
+
+/**
+ * _sqrt_resursion - returns natural square root of a number
+ * @n: integar input number
+ * Return: natural square root or -1 if no square root of n
+**/
+int _sqrt_recursion(int n)
+{
+	return(natural_square(n, 1));
+}
+
+
+/**
+ * natural_square - A FUNC. THAT HELP TP FIND THE SQUARE ROOT
+ * @x: integar input
+ * @y: integar input
+ * Return: square root or -1
+**/
+int natural_square(int x, int y)
+{
+	int s = y * y;
+	if (s > x)
+	{
+		return (-1);
+	}
+	if (s == x)
+	{
+		return (y);
+	}
+	return (natural_square(x, y + 1));
+} 
